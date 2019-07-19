5e28c18431842e44abe9541c860bf8e681a3ab00
Alexander Rau <alexanderrau86@yahoo.com>
2019-07-18 19:01:01 -0600

    Start building landing page and initial folders with router and redux

---
+++ src/components/LandingPage/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './LandingPage.css';
+
+export class LandingPage extends Component {
+  constructor() {
+    super();
+
+  }
+
+  render() {
+    return(
+      <div className='LandingPage'>
+        <h1>CRRT Teaching Tool v.2!!</h1>
+      </div>
+    )
+  }
+}
+
+export default LandingPage;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>, document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
