1d6a807b0a032c048d362c2a744109c8524041e1
Alexander Rau <alexanderrau86@yahoo.com>
2019-07-18 19:34:19 -0600

    build landing page and home route

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
 import { Route, withRouter, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
 import LandingPage from '../components/LandingPage';
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
+      <Switch>
+        <Route exact path='/' component={LandingPage} />
+      </Switch>
     </div>
   );
 }
 
-export default withRouter(App);
+export default withRouter(connect(null, null)(App));
