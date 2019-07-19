600aba154a1d23f603b403f282f9cd6a348b14ce
Alexander Rau <alexanderrau86@yahoo.com>
2019-07-18 19:28:18 -0600

    Set up redux and thunks

--- src/index.js
+++ src/index.js
@@ -7,12 +7,17 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './Reducers';
 import thunk from 'redux-thunk';
 
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>, document.getElementById('root')
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
