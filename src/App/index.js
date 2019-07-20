b77354c48208f181a4389d2650df9234cd261c16
Alexander Rau <alexanderrau86@yahoo.com>
2019-07-20 11:53:21 -0600

    Build NavLink that leads to Simulator page and route

--- src/App/index.js
+++ src/App/index.js
@@ -3,13 +3,14 @@ import './App.css';
 import { Route, withRouter, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import LandingPage from '../components/LandingPage';
+import Simulator from '../containers/Simulator';
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path='/' component={LandingPage} />
-        <Route exact path="/simulator" component={} />
+        <Route exact path="/simulator" component={Simulator} />
       </Switch>
     </div>
   );
