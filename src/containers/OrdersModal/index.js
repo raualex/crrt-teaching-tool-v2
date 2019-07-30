ee3274c24842e140285a7b3f9e110489931adb59
Alexander Ela <ela.alexander@gmail.com>
2019-07-29 18:42:03 -0600

    Add handleChange() and submitOrder()

--- src/containers/OrdersModal/index.js
+++ src/containers/OrdersModal/index.js
@@ -21,25 +21,67 @@ class OrdersModal extends Component {
 		}
 	}
 
+	handleChange = event => {
+		const { name, value } = event.target
+		this.setState({ [name]: value })
+	}
+
+	submitOrder = event => {
+			
+	}
+
 	render() {
+		const {
+			modality,
+			sodium,
+			potassium,
+			chloride,
+			bicarbonate,
+			calcium,
+			magnesium,
+			phosphorous ,
+			grossUltraFiltration,
+			bloodFlowRate,
+			replacementFluidFlowRate,
+			otherFluidsMedications,
+			anticoagulation
+		} = this.state
+
 		return (
-			<form class='OrdersModal'>
-				<header class='orders-header'>
+			<form className='OrdersModal'>
+				<header className='orders-header'>
 				</header>
 
-				<section class='orders-modality-container'>
+				<section className='orders-modality-container'>
 				</section>
 
-				<section class='orders-replacement-fluid-container'>
+				<section className='orders-replacement-fluid-container'>
+					<h3>Replacement Fluid</h3>
+					<article className='input-container'>
+						<h4>Sodium</h4>
+						<div className='medication-info-button'></div>
+						<input 
+							type='number'
+							placeholder='Sodium'
+							name='sodium'
+							value={sodium}
+							onChange={event => this.handleChange(event)}
+						/>
+					</article>
 				</section>
 
-				<section class='orders-modality-other-container'>
+				<section className='orders-modality-other-container'>
 				</section>
 
-				<section class='orders-modality-anticoagulation-container'>
+				<section className='orders-modality-anticoagulation-container'>
 				</section>
 
-				<footer class='orders-header'>
+				<button 
+					className='submit-case-btn' onClick={event => this.submitOrder(event)}>
+					Submit Order
+				</button>
+
+				<footer className='orders-header'>
 				</footer>
 			</form>
 		)
