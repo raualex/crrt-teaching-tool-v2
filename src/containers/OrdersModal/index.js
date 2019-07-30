acf5fc371e556afc7f352a9e5f3f8eea98c34059
Alexander Ela <ela.alexander@gmail.com>
2019-07-29 18:24:59 -0600

    Add medications names to state

--- src/containers/OrdersModal/index.js
+++ src/containers/OrdersModal/index.js
@@ -5,7 +5,19 @@ class OrdersModal extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-
+			modality: '',
+			sodium: 0,
+			potassium: 0,
+			chloride: 0,
+			bicarbonate: 0,
+			calcium: 0,
+			magnesium: 0,
+			phosphorous : 0,
+			grossUltraFiltration: 0,
+			bloodFlowRate: 0,
+			replacementFluidFlowRate: 0,
+			otherFluidsMedications: '',
+			anticoagulation: ''
 		}
 	}
 
@@ -14,19 +26,19 @@ class OrdersModal extends Component {
 			<form class='OrdersModal'>
 				<header class='orders-header'>
 				</header>
-				
+
 				<section class='orders-modality-container'>
 				</section>
-				
+
 				<section class='orders-replacement-fluid-container'>
 				</section>
-				
+
 				<section class='orders-modality-other-container'>
 				</section>
-				
+
 				<section class='orders-modality-anticoagulation-container'>
 				</section>
-				
+
 				<footer class='orders-header'>
 				</footer>
 			</form>
