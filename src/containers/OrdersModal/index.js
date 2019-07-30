e3988d9f5e5e3a57aa115d05d597d13449847c8c
Alexander Ela <ela.alexander@gmail.com>
2019-07-29 18:55:33 -0600

    Add inputs for all Replacement Fluids

--- src/containers/OrdersModal/index.js
+++ src/containers/OrdersModal/index.js
@@ -27,7 +27,27 @@ class OrdersModal extends Component {
 	}
 
 	submitOrder = event => {
-			
+		event.preventDefault();
+		//Add this.state to Redux
+		//MapDispatchToProps	
+	}
+
+	clearInputs = () => {
+		this.setState({
+			modality: '',
+			sodium: 0,
+			potassium: 0,
+			chloride: 0,
+			bicarbonate: 0,
+			calcium: 0,
+			magnesium: 0,
+			phosphorous : 0,
+			grossUltraFiltration: 0,
+			bloodFlowRate: 0,
+			replacementFluidFlowRate: 0,
+			otherFluidsMedications: '',
+			anticoagulation: ''
+		})
 	}
 
 	render() {
@@ -62,9 +82,107 @@ class OrdersModal extends Component {
 						<div className='medication-info-button'></div>
 						<input 
 							type='number'
-							placeholder='Sodium'
 							name='sodium'
 							value={sodium}
+							onChange={event => this.handleChange(event)}
+						/>
+					</article>
+
+					<article className='input-container'>
+						<h4>Potassium</h4>
+						<div className='medication-info-button'></div>
+						<input 
+							type='number'
+							name='potassium'
+							value={potassium}
+							onChange={event => this.handleChange(event)}
+						/>
+					</article>
+
+					<article className='input-container'>
+						<h4>Chloride</h4>
+						<div className='medication-info-button'></div>
+						<input 
+							type='number'
+							name='chloride'
+							value={chloride}
+							onChange={event => this.handleChange(event)}
+						/>
+					</article>
+
+					<article className='input-container'>
+						<h4>Bicarbonate</h4>
+						<div className='medication-info-button'></div>
+						<input 
+							type='number'
+							name='bicarbonate'
+							value={bicarbonate}
+							onChange={event => this.handleChange(event)}
+						/>
+					</article>
+
+					<article className='input-container'>
+						<h4>Calcium</h4>
+						<div className='medication-info-button'></div>
+						<input 
+							type='number'
+							name='calcium'
+							value={calcium}
+							onChange={event => this.handleChange(event)}
+						/>
+					</article>
+
+					<article className='input-container'>
+						<h4>Magnesium</h4>
+						<div className='medication-info-button'></div>
+						<input 
+							type='number'
+							name='magnesium'
+							value={magnesium}
+							onChange={event => this.handleChange(event)}
+						/>
+					</article>
+
+					<article className='input-container'>
+						<h4>Phosphorous</h4>
+						<div className='medication-info-button'></div>
+						<input 
+							type='number'
+							name='phosphorous'
+							value={phosphorous}
+							onChange={event => this.handleChange(event)}
+						/>
+					</article>
+
+					<article className='input-container'>
+						<h4>Gross Ultrafiltration (ml/Hr)</h4>
+						<div className='medication-info-button'></div>
+						<input 
+							type='number'
+							name='grossUltraFiltration'
+							value={grossUltraFiltration}
+							onChange={event => this.handleChange(event)}
+						/>
+					</article>
+
+					<article className='input-container'>
+						<h4>Blood Flow Rate (mL/min)</h4>
+						<div className='medication-info-button'></div>
+						<input 
+							type='number'
+							name='bloodFlowRate'
+							value={bloodFlowRate}
+							onChange={event => this.handleChange(event)}
+						/>
+					</article>
+
+					<article className='input-container'>
+						<h4>Replacement Fluid Flow Rate (L/hr)</h4>
+						<div className='medication-info-button'></div>
+						<input 
+							type='number'
+							name='replacementFluidFlowRate'
+							value={replacementFluidFlowRate}
 							onChange={event => this.handleChange(event)}
 						/>
 					</article>
@@ -82,6 +200,7 @@ class OrdersModal extends Component {
 				</button>
 
 				<footer className='orders-header'>
+					<button className='clear-order-inputs-btn' onClick={this.clearInputs}></button>
 				</footer>
 			</form>
 		)
