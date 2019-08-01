91197c7e58769d3ca94651a321e18eab74374ef8
Alexander Rau <alexanderrau86@yahoo.com>
2019-08-01 17:24:04 -0600

    Add data output modal elements and make them dynamic to what button has been selected

--- src/containers/Simulator/index.js
+++ src/containers/Simulator/index.js
@@ -2,12 +2,43 @@ import React from 'react';
 import './Simulator.css';
 import { connect } from 'react-redux';
 import { setSelectedModal } from '../../Actions/selection-actions';
+import DataOutputModal from '../DataOutputModal';
 
 function Simulator(props) {
-  let { setSelectedModal } = props
-
-  return(
-    <div>
+  let { setSelectedModal, selectedModal } = props
+  
+  if (selectedModal === '') {
+    return(
+      <div>
+        <h1>CRRT SIMULATOR!</h1>
+        <div className='modalBtnContainer'>
+          <button 
+            onClick={() => setSelectedModal('History of Present Illness')}
+          >History of Present Illness</button>
+          <button 
+            onClick={() => setSelectedModal('Input/Output')}
+          >Input/Output</button>
+          <button 
+            onClick={() => setSelectedModal('Vitals')}
+          >Vitals</button>
+          <button 
+            onClick={() => setSelectedModal('Laboratory Data')}
+          >Laboratory Data</button>
+          <button 
+            onClick={() => setSelectedModal('Medications')}
+          >Medications</button>
+          <button 
+            onClick={() => setSelectedModal('Imaging')}
+          >Imaging</button>
+          <button 
+            onClick={() => setSelectedModal('Physical Exam')}
+          >Physical Exam</button>
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div>
       <h1>CRRT SIMULATOR!</h1>
       <div className='modalBtnContainer'>
         <button 
@@ -32,12 +63,18 @@ function Simulator(props) {
           onClick={() => setSelectedModal('Physical Exam')}
         >Physical Exam</button>
       </div>
+      <DataOutputModal />
     </div>
-  )
+    )
+  }
 }
+
+export const mapStateToProps = (state) => ({
+  selectedModal: state.selectedModal
+})
 
 export const mapDispatchToProps = (dispatch) => ({
   setSelectedModal: (modal) => dispatch(setSelectedModal(modal))
 })
 
-export default connect(null, mapDispatchToProps)(Simulator);
+export default connect(mapStateToProps, mapDispatchToProps)(Simulator);
