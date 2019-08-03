3e55e80c97ef71317ec6bc3519f6ec829e3b3c00
Alexander Ela <ela.alexander@gmail.com>
2019-08-03 16:33:01 -0600

    Add conditional rendering to InputCard to render text or radio input based on type

--- src/containers/InputCard/index.js
+++ src/containers/InputCard/index.js
@@ -15,30 +15,56 @@ class InputCard extends Component {
 		const { errorMessages, dosageNames } = orderDosages;
 
 		return (
-			<article className='input-container'>
-				<div className='header-info-container'>
-					<h4>{ dosageNames[medicine] }</h4>
-					<a 
-						href='https://github.com/raualex/crrt-teaching-tool-v2' 
-						className='textbook-link'
-					>
-						<i className='far fa-question-circle'></i>
-					</a>
-				</div>
+				<article className='input-container'>
+					{ type === 'text' &&
+							<div className='InputCard-text'>
+								<div className='header-info-container'>
+									<h4>{ dosageNames[medicine] }</h4>
+									<a 
+										href='https://github.com/raualex/crrt-teaching-tool-v2' 
+										className='textbook-link'
+									>
+										<i className='far fa-question-circle'></i>
+									</a>
+								</div>
 
-				<input 
-					type='text'
-					name={medicine}
-					value={isNaN(enteredInput) ? 0 : enteredInput}
-					onChange={event => handleNumberChange(event)}
-				/>
-				<div className='input-error-container'>
-					<p className='input-error-text'>{
-						dosageErrors.includes(medicine) ? errorMessages[medicine] : ''
+								<input 
+									type='text'
+									name={medicine}
+									value={isNaN(enteredInput) ? 0 : enteredInput}
+									onChange={event => handleNumberChange(event)}
+								/>
+								<div className='input-error-container'>
+									<p className='input-error-text'>{
+										dosageErrors.includes(medicine) ? errorMessages[medicine] : ''
+									}
+									</p>
+								</div>
+							</div>
 					}
-					</p>
-				</div>
-			</article>
+
+					{
+						type === 'radio' &&
+							<div className='modality-radio'>
+								<label>
+									<input 
+										type='radio'
+										name='modality'
+										value='Pre-filter CVVH'
+										checked={modality === 'Pre-filter CVVH'}
+										onChange={this.handleStringChange}
+									/>
+									Pre-filter CVVH
+										<a 
+										href='https://github.com/raualex/crrt-teaching-tool-v2' 
+										className='textbook-link'
+										>
+											<i className='far fa-question-circle'></i>
+										</a>
+								</label>
+							</div>						
+					}
+				</article>
 		)
 	}
 }
