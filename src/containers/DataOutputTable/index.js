ce2ef5a5cc26b4ea0f24e8282e9462fbabbd5289
Alexander Rau <alexanderrau86@yahoo.com>
2019-08-20 04:12:38 -0600

    psuedo code the solution to the number of columnsquestion

--- src/containers/DataOutputTable/index.js
+++ src/containers/DataOutputTable/index.js
@@ -6,12 +6,18 @@ const uuidv4 = require('uuid/v4')
 
 export class DataOutputTable extends Component {
 
+  //write function that accesses the arrays for each output data in Redux:
+  //i.e., each DataOutputTable will have an object in Redux, with each key representing a different row in that modal
+  //each key/row name will have a value of an array of numbers, map over that array and print a `<td></td>` 
+  //element with that number for every value in the array, and add it to the modalTableRowKeys element in the map below,
+  //right after the <td> with the key name in it
+
   render() {
     let { selectedModal } = this.props
     let modalNameForClass;
     let modalNameForKeys;
     let modalTableRowKeys;
-    let rowsNumber = 0;
+    let rowsNumber = 0; //might not need this
 
     if (
       selectedModal === 'Laboratory Data' || 
@@ -22,15 +28,18 @@ export class DataOutputTable extends Component {
       modalNameForClass = selectedModal.replace(/\s/g, '-')
       modalNameForKeys = selectedModal.replace(/\s/g, '')
       modalTableRowKeys = modalKeys[modalNameForKeys].map((keyName, index) => {
-          rowsNumber = rowsNumber + 1
-          return <tr key={uuidv4()}><td key={uuidv4()} className={'table-key index'+index}>{keyName}</td></tr>
+          rowsNumber = rowsNumber + 1 //might not need this
+          return <tr key={uuidv4()}><td key={uuidv4()} className={'table-key index'+index}>{keyName}</td>{/*new <td>s go here*/}</tr>
       });
 
       return(
         <table className={'dataot-' + modalNameForClass}>
           <thead>
             <tr>
-              <th className='blank-table-head'></th>
+              <th className='blank-table-head'></th>{/*here, add a function that takes the current time in Redux
+               and adds the appropriate number of columns needed, with the incremented up time stamps, based off of 
+               the eight hour peroid you are in, via Redux (and correct setTime Redux function to go by 12 hour time
+                in 8 hour increments)*/}
             </tr>
           </thead>
           <tbody>
