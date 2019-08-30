7182eb35c74ab64970174721df544533ecb98040
Alexander Rau <alexanderrau86@yahoo.com>
2019-08-29 21:54:38 -0600

    Add CaseSelectionModal snapshot test so Travis will pass

---
+++ src/containers/CaseSelectionModal/CaseSelectionModal.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { CaseSelectionModal } from './';
+import { shallow } from 'enzyme';
+
+describe('CaseSelectionModal', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<CaseSelectionModal />)
+  });
+
+  it('matches the snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
+  });
+
+});
--- src/containers/CaseSelectionModal/index.js
+++ src/containers/CaseSelectionModal/index.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 
 export class CaseSelectionModal extends Component {
 
-  redner() {
+  render() {
     return(
       <div>
         <h1>SELECT A CASE, BITCH!</h1>
