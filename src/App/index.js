eede4988010f45450f4da7f450d7370a5b1b96aa
Alexander Rau <alexanderrau86@yahoo.com>
2019-08-30 13:27:31 -0600

    Build out case number selection element and have it assign the selected case to Redux

--- src/App/index.js
+++ src/App/index.js
@@ -17,12 +17,19 @@ export class App extends Component {
 
   render() {
     let { selectedCase } = this.props
+    let neededComponent;
+
+    if (selectedCase.id === undefined) {
+      neededComponent = CaseSelectionModal
+    } else {
+      neededComponent = Simulator
+    }
 
     return (
       <div className="App">
         <Switch>
           <Route exact path='/' component={LandingPage} />
-          <Route exact path="/simulator" component={selectedCase === {} ? CaseSelectionModal : Simulator} />
+          <Route exact path="/simulator" component={neededComponent} />
           <Route exact path="/textbook" component={Textbook} />
           <Route exect path="/calculator" component={Calculator} />
         </Switch>
@@ -39,4 +46,4 @@ export const mapDispatchToProps = (dispatch) => ({
   fetchCases: () => dispatch(fetchCases())
 })
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
--- src/containers/CaseSelectionModal/index.js
+++ src/containers/CaseSelectionModal/index.js
@@ -1,16 +1,55 @@
 import React, { Component } from 'react';
 import './CaseSelectionModal.css';
 import { connect } from 'react-redux';
+import { selectActiveCase } from '../../Actions/case-actions';
 
 export class CaseSelectionModal extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      caseNumber: undefined
+    }
+  }
+
+  handleChange = (event) => {
+    let { value } = event.target
+    this.setState({caseNumber: parseInt(value)})
+  }
+
+  handleClick = (event) => {
+    let { caseNumber } = this.state
+    let { allCases, selectActiveCase } = this.props
+    let selectedCase;
+
+    event.preventDefault()
+    selectedCase = allCases.filter((patientCase) => {
+      console.log(patientCase.id)
+      console.log(caseNumber)
+      return patientCase.id === caseNumber
+    })
+    selectActiveCase(selectedCase)
+  }
 
   render() {
     return(
-      <div>
-        <h1>SELECT A CASE, BITCH!</h1>
+      <div className='csm-main-container'>
+        <h1>Select a Case ID for the Simulator</h1>
+        <form>
+          <input onChange={this.handleChange}></input>
+          <button onClick={this.handleClick}>Submit</button>
+        </form>
       </div>
     )
   }
 }
 
-export default connect(null, null)(CaseSelectionModal)
+export const mapStateToProps = (state) => ({
+  allCases: state.allCases
+})
+
+export const mapDispatchToProps = (dispatch) => ({
+  selectActiveCase: (selectedCase) => dispatch(selectActiveCase(selectedCase))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CaseSelectionModal)
