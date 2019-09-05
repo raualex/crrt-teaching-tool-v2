acc6a211e7b46bb094186ddb1dc1abbe4b6319d2
Alexander Ela <ela.alexander@gmail.com>
2019-09-04 23:13:10 -0600

    Spread existing state for each med into updatedLabResults

--- src/Reducers/calculation-reducers.js
+++ src/Reducers/calculation-reducers.js
@@ -16,22 +16,22 @@ export const calculateLabDataReducer = (state = defaultLabDataState, action) => 
   const { initialValue, time } = action
   switch(action.type) {
     case 'CALCULATE_LAB_DATA':
-    // map through labDataValues
-    //run calculateLabs on each
-    // for each return an object with name and labResult
-    //spread returned value for each into corresponding array
+      // map through labDataValues
+      //run calculateLabs on each
+      // for each return an object with name and labResult
+      //spread returned value for each into corresponding array
 
-    const labDataResults = labDataValues.reduce((updatedLabResults, med) => {
-      const { name, dialysate, effluentFlowRate, volumeOfDistribution, productionRate } = med
-      const { weight } = state.selectedCase
-      const labResult = calculateLab(initialValue, dialysate, effluentFlowRate, time, weight, volumeOfDistribution, productionRate)
-      updatedLabResults[name] = [...labResult]
-      return updatedLabResults
-    }, {})
-      return {
-        ...state,
-        labDataResults
-      }
+      const labDataResults = labDataValues.reduce((updatedLabResults, med) => {
+        const { name, dialysate, effluentFlowRate, volumeOfDistribution, productionRate } = med
+        const { weight } = state.selectedCase
+        const labResult = calculateLab(initialValue, dialysate, effluentFlowRate, time, weight, volumeOfDistribution, productionRate)
+        
+        updatedLabResults[name] = [...state[name], labResult]
+        
+        return updatedLabResults
+      }, {})
+      
+      return labDataResults
     default:
       return state
   }
