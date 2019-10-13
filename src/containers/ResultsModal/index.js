71a6dde98f3c8956f2b9aa04dc0d1ad0e2d0d0bd
Alexander Rau <alexanderrau86@yahoo.com>
2019-10-12 22:00:40 -0600

    Finish initial css on ResultsModal

--- src/containers/ResultsModal/index.js
+++ src/containers/ResultsModal/index.js
@@ -15,7 +15,7 @@ export class ResultsModal extends Component {
         </div>
         <div className='rm-body-container'>
           <h3 className='rm-body-section-title'>Overall Result</h3>
-          <p>Your patient has...</p>
+          <p className='rm-body-msg'>Your patient has...</p>
           <h3 className='rm-body-section-title'>
             Dose
             <a
@@ -27,9 +27,9 @@ export class ResultsModal extends Component {
             <i className="far fa-question-circle"></i>
             </a>
           </h3>
-          <p># earned out of a possible #</p>
-          <p>The average dose delivered was: #</p>
-          <p>Kidney Disease: Improving Global Outcomes/KDIGO recommends an average dose of 20 – 25 mL/kg/hr. Other experts recommend maintaining a "dosing window" of 20-40 mL/kg/hr. See the Dosing section in the written material for more information.</p>
+          <p className='rm-body-msg'># earned out of a possible #</p>
+          <p className='rm-body-msg'>The average dose delivered was: #</p>
+          <p className='rm-body-msg'>Kidney Disease: Improving Global Outcomes/KDIGO recommends an average dose of 20 – 25 mL/kg/hr. Other experts recommend maintaining a "dosing window" of 20-40 mL/kg/hr. See the Dosing section in the written material for more information.</p>
           <h3 className='rm-body-section-title'>
             Filter Life
             <a
@@ -41,10 +41,10 @@ export class ResultsModal extends Component {
             <i className="far fa-question-circle"></i>
             </a>
           </h3>
-          <p># earned out of a possible #</p>
-          <p>You used # over the course of the simulation. The average filter life was # hours</p>
-          <p>Your average filtration fraction was #</p>
-          <p>Filtration Fraction measures how much the plasma entering the filter is concentrated by ultrafiltration. It should be kept below 25% to reduce clotting. See the Non-pharmacologic approach to clotting for more information.</p>
+          <p className='rm-body-msg'># earned out of a possible #</p>
+          <p className='rm-body-msg'>You used # over the course of the simulation. The average filter life was # hours</p>
+          <p className='rm-body-msg'>Your average filtration fraction was #</p>
+          <p className='rm-body-msg'>Filtration Fraction measures how much the plasma entering the filter is concentrated by ultrafiltration. It should be kept below 25% to reduce clotting. See the Non-pharmacologic approach to clotting for more information.</p>
           <h3 className='rm-body-section-title'>
             Electrolytes
             <a
@@ -56,13 +56,13 @@ export class ResultsModal extends Component {
             <i className="far fa-question-circle"></i>
             </a>
           </h3>
-          <p># earned out of a possible #</p>
-          <p>Final sodium score was #</p>
-          <p>Final potassium score was #</p>
-          <p>Final calcium score was #</p>
-          <p>Final magnesium score was #</p>
-          <p>Final phosphorous score was #</p>
-          <p>One of the goals of CRRT is to normalize electrolyte values. See Electrolytes for an in-depth discussion about how to do this in CRRT.</p>
+          <p className='rm-body-msg'># earned out of a possible #</p>
+          <p className='rm-body-msg'>Final sodium score was #</p>
+          <p className='rm-body-msg'>Final potassium score was #</p>
+          <p className='rm-body-msg'>Final calcium score was #</p>
+          <p className='rm-body-msg'>Final magnesium score was #</p>
+          <p className='rm-body-msg'>Final phosphorous score was #</p>
+          <p className='rm-body-msg'>One of the goals of CRRT is to normalize electrolyte values. See Electrolytes for an in-depth discussion about how to do this in CRRT.</p>
           <h3 className='rm-body-section-title'>
             Acid/Base
             <a
@@ -74,11 +74,11 @@ export class ResultsModal extends Component {
             <i className="far fa-question-circle"></i>
             </a>
           </h3>
-          <p># earned out of a possible #</p>
-          <p>Final pH was #</p>
-          <p>Lowest pH was #</p>
-          <p>Highest pH was #</p>
-          <p>The goal in CRRT is to correct acidosis, and then to maintain the patient within a normal pH range. See Acid-Base for more information.</p>
+          <p className='rm-body-msg'># earned out of a possible #</p>
+          <p className='rm-body-msg'>Final pH was #</p>
+          <p className='rm-body-msg'>Lowest pH was #</p>
+          <p className='rm-body-msg'>Highest pH was #</p>
+          <p className='rm-body-msg'>The goal in CRRT is to correct acidosis, and then to maintain the patient within a normal pH range. See Acid-Base for more information.</p>
           <h3 className='rm-body-section-title'>
             Volume
             <a
@@ -90,11 +90,11 @@ export class ResultsModal extends Component {
             <i className="far fa-question-circle"></i>
             </a>
           </h3>
-          <p># earned out of a possible #</p>
-          <p>The patient's cumulative change in volume was # -- Initial weight: #, Final weight: #</p>
-          <p>The patient started the case at #% overload</p>
-          <p>At 48 hours, the patient was #% overloaded</p>
-          <p>At 72 hours, the patient was #% overloaded</p>
+          <p className='rm-body-msg'># earned out of a possible #</p>
+          <p className='rm-body-msg'>The patient's cumulative change in volume was # -- Initial weight: #, Final weight: #</p>
+          <p className='rm-body-msg'>The patient started the case at #% overload</p>
+          <p className='rm-body-msg'>At 48 hours, the patient was #% overloaded</p>
+          <p className='rm-body-msg'>At 72 hours, the patient was #% overloaded</p>
         </div>
       </div>
     )
