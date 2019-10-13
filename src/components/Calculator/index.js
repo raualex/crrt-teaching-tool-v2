019739aab63cbaaf6ce6ab574bb493cac68845a5
Alexander Rau <alexanderrau86@yahoo.com>
2019-10-12 22:23:00 -0600

    Add Under Construction messages to unfinished component pages

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -4,7 +4,8 @@ import "./Calculator.css";
 function Calculator() {
   return (
     <div>
-      <h1>CRRT Calculator!</h1>
+      <h1>CRRT Calculator</h1>
+      <h3>-- Currently Under Construction --</h3>
     </div>
   );
 }
