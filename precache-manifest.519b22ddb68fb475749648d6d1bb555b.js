87608dc727e8d4027bcc8c0c3296cf831d8f8ac3
Alexander Rau <alexanderrau86@yahoo.com>
2019-10-20 18:50:51 -0600

    Updates

---
+++ precache-manifest.519b22ddb68fb475749648d6d1bb555b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32c4b25bb6c9e653b5f66c26297368a3",
+    "url": "/crrt-teaching-tool-v2/index.html"
+  },
+  {
+    "revision": "27c5a143bd6615f8f749",
+    "url": "/crrt-teaching-tool-v2/static/css/main.863cbb81.chunk.css"
+  },
+  {
+    "revision": "8b605658ce3407c3a910",
+    "url": "/crrt-teaching-tool-v2/static/js/2.777cfda1.chunk.js"
+  },
+  {
+    "revision": "27c5a143bd6615f8f749",
+    "url": "/crrt-teaching-tool-v2/static/js/main.972490b1.chunk.js"
+  },
+  {
+    "revision": "b8fbdf112bf6d91da756",
+    "url": "/crrt-teaching-tool-v2/static/js/runtime~main.f3c9453c.js"
+  }
+]);
